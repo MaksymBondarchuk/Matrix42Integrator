fa6dce2ca2b4b63e52c5b6a647e6c85ea7ac4e0d
MaxBondarchuk <bondarchuk.m.y@gmail.com>
2018-06-09 12:00:39 +0300

    Add project files.

---
+++ Matrix42Integrator/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Matrix42Integrator.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
