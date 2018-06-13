e8be962eeaeda62263f15d15ac39c891f0fbabcd
Maksym Bondarchuk <bondarchuk.m.y@gmail.com>
2018-06-13 13:35:19 +0300

    Working Authentication

--- Matrix42Integrator/Controllers/AccountController.cs
+++ Matrix42Integrator/Controllers/AccountController.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.IO;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Security.Claims;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
@@ -19,7 +15,6 @@ namespace Matrix42Integrator.Controllers
 	[Route("[controller]/[action]")]
 	public class AccountController : Controller
 	{
-		private HttpClient HttpClient { get; } = new HttpClient();
 		private readonly IHostingEnvironment _hostingEnvironment;
 
 		public AccountController(IHostingEnvironment hostingEnvironment)
@@ -29,56 +24,23 @@ namespace Matrix42Integrator.Controllers
 
 
 		[HttpGet]
-		public IActionResult Login(string returnUrl = "/")
+		public IActionResult Login()
 		{
 			return RedirectToAction("Authorize");
-			//return Challenge(new AuthenticationProperties() { RedirectUri = returnUrl });
 		}
 
-		public async Task<IActionResult> Authorize()
+		public IActionResult Authorize()
 		{
 			return Redirect("https://accounts.matrix42.com/issue/oauth2/authorize?client_id=cf9ac74c-fefc-4d1c-9fcb-0c5cecd5203d&scope=urn%3a0580150c-7d26-422e-983c-53b211c44a3e&redirect_uri=http%3a%2f%2flocalhost%3a27923%2faccount%2fgettoken&response_type=token");
 		}
 
+		// ReSharper disable once InconsistentNaming
 		public async Task<IActionResult> GetToken(string access_token)
 		{
 			ClaimsPrincipal principal = ValidateToken(access_token);
 
-			return SignIn(principal, JwtBearerDefaults.AuthenticationScheme);
-
-			//if (token != null)
-			//{
-			//	var userIdentity = new ClaimsIdentity("Identity")
-			//	{
-			//		Label = "Identity"
-			//	};
-			//	userIdentity.AddClaims(token.Claims);
-			//	//userIdentity.AddClaim(new Claim(ClaimTypes.Name, token.Claims));
-			//	//userIdentity.AddClaim(new Claim(ClaimTypes.NameIdentifier, "1"));
-			//	//userIdentity.AddClaim(new Claim(ClaimTypes.Country, "PL"));
-			//	//userIdentity.AddClaim(new Claim(ClaimTypes.Email, "sampleuser@test.pl"));
-			//	//userIdentity.AddClaim(new Claim(ClaimTypes.Role, "Admin"));
-			//	//userIdentity.AddClaim(new Claim(ClaimTypes.Role, "User"));
-			//	var identity = new ClaimsPrincipal(userIdentity);
-
-			//	return SignIn(identity, JwtBearerDefaults.AuthenticationScheme);
-			//}
-			// https://accounts.matrix42.com/issue/oauth2/authorize?client_id=cf9ac74c-fefc-4d1c-9fcb-0c5cecd5203d&scope=urn%3a0580150c-7d26-422e-983c-53b211c44a3e&redirect_uri=http%3a%2f%2flocalhost%3a27923%2foauth2%2fcallback&response_type=token
-
-			var request = new HttpRequestMessage(HttpMethod.Get, "https://accounts.matrix42.com/issue/oauth2/authorize?client_id=cf9ac74c-fefc-4d1c-9fcb-0c5cecd5203d&scope=urn%3a0580150c-7d26-422e-983c-53b211c44a3e&redirect_uri=http%3a%2f%2flocalhost%3a27923%2foauth2%2fcallback&response_type=token");
-			request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-			//request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
-
-			var response = await HttpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
-			response.EnsureSuccessStatusCode();
-
-			var data = await response.Content.ReadAsStringAsync();
-
-			//var user = JObject.Parse(await response.Content.ReadAsStringAsync());
-
-			//context.RunClaimActions(user);
-			//return data;
-			return null;
+			await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+			return Redirect("/");
 		}
 
 		private ClaimsPrincipal ValidateToken(string token)
@@ -103,10 +65,7 @@ namespace Matrix42Integrator.Controllers
 
 			var principal = handler.ValidateToken(token, validationParameters, out SecurityToken securityToken);
 
-			//Debugger.Break();
 			return principal;
-
-			//return securityToken.ValidFrom <= DateTime.Now && DateTime.Now <= securityToken.ValidTo;
 		}
 	}
 }
--- Matrix42Integrator/Startup.cs
+++ Matrix42Integrator/Startup.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -20,101 +23,8 @@ namespace Matrix42Integrator
 		{
 			services.AddMvc();
 
-			//services.AddAuthentication(options =>
-			//{
-			//	options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-			//	options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-			//	options.DefaultChallengeScheme = "Matrix42";
-			//})
-			//.AddCookie()
-			//.AddOAuth("Matrix42", options =>
-			//{
-			//	options.ClientId = Configuration["Matrix42:ClientId"];
-			//	options.ClientSecret = Configuration["Matrix42:ClientSecret"];
-			//	options.Scope.Add(Configuration["Matrix42:Scope"]);
-			//	options.CallbackPath = new PathString("/account/gettoken");
-
-			//	options.AuthorizationEndpoint = "https://accounts.matrix42.com/issue/oauth2/authorize";
-			//	options.TokenEndpoint = "https://accounts.matrix42.com/issue/oauth2/token";
-			//	options.UserInformationEndpoint = "https://accounts.matrix42.com/my/account";
-
-			//	options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
-			//	options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
-			//	options.ClaimActions.MapJsonKey("urn:github:login", "login");
-			//	options.ClaimActions.MapJsonKey("urn:github:url", "html_url");
-			//	options.ClaimActions.MapJsonKey("urn:github:avatar", "avatar_url");
-
-			//	options.Events = new OAuthEvents
-			//	{
-			//		OnCreatingTicket = async context =>
-			//		{
-			//			var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
-			//			request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-			//			request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
-
-			//			var response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
-			//			response.EnsureSuccessStatusCode();
-
-			//			var user = JObject.Parse(await response.Content.ReadAsStringAsync());
-
-			//			context.RunClaimActions(user);
-			//		},
-			//		OnTicketReceived = context =>
-			//		{
-			//			Debugger.Break();
-			//			throw new NotImplementedException();
-			//		}
-			//	};
-
-			//	options.Validate();
-			//});
-
-			//services.AddAuthentication(options =>
-			//{
-			//	options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-			//	options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-			//	options.DefaultChallengeScheme = "GitHub";
-			//})
-			//.AddCookie()
-			//.AddOAuth("GitHub", options =>
-			//{
-			//	options.ClientId = Configuration["GitHub:ClientId"];
-			//	options.ClientSecret = Configuration["GitHub:ClientSecret"];
-			//	options.CallbackPath = new PathString("/signin-github");
-
-			//	options.AuthorizationEndpoint = "https://github.com/login/oauth/authorize";
-			//	options.TokenEndpoint = "https://github.com/login/oauth/access_token";
-			//	options.UserInformationEndpoint = "https://api.github.com/user";
-
-			//	options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
-			//	options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
-			//	options.ClaimActions.MapJsonKey("urn:github:login", "login");
-			//	options.ClaimActions.MapJsonKey("urn:github:url", "html_url");
-			//	options.ClaimActions.MapJsonKey("urn:github:avatar", "avatar_url");
-
-			//	options.Events = new OAuthEvents
-			//	{
-			//		OnCreatingTicket = async context =>
-			//		{
-			//			var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
-			//			request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-			//			request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
-
-			//			var response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
-			//			response.EnsureSuccessStatusCode();
-
-			//			var user = JObject.Parse(await response.Content.ReadAsStringAsync());
-
-			//			context.RunClaimActions(user);
-			//		}
-			//	};
-			//});
-
-			services.AddAuthentication(options =>
-			{
-				options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-				options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
-			});
+			services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+				.AddCookie();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
