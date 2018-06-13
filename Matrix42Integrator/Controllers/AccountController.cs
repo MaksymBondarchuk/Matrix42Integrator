f4dcee4a87e4f174e26f8a2fb8ae6d280dbb5b10
MaxBondarchuk <bondarchuk.m.y@gmail.com>
2018-06-13 23:56:46 +0300

    Without access_token hardcode

--- Matrix42Integrator/Controllers/AccountController.cs
+++ Matrix42Integrator/Controllers/AccountController.cs
@@ -1,10 +1,11 @@
-﻿using Matrix42Integrator.SecurityLayer;
+﻿using System.Collections.Generic;
+using Matrix42Integrator.SecurityLayer;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 namespace Matrix42Integrator.Controllers
 {
@@ -14,7 +15,7 @@ namespace Matrix42Integrator.Controllers
 		[HttpGet]
 		public IActionResult SignIn()
 		{
-			return RedirectToAction("Authorize");
+			return RedirectToAction("Matrix42SignIn");
 		}
 
 		[HttpGet]
@@ -24,7 +25,7 @@ namespace Matrix42Integrator.Controllers
 			return Redirect("/");
 		}
 
-		public IActionResult Authorize()
+		public IActionResult Matrix42SignIn()
 		{
 			return Redirect("https://accounts.matrix42.com/issue/oauth2/authorize?client_id=cf9ac74c-fefc-4d1c-9fcb-0c5cecd5203d&scope=urn%3a0580150c-7d26-422e-983c-53b211c44a3e&redirect_uri=http%3a%2f%2flocalhost%3a27923%2faccount%2fgettoken&response_type=token");
 		}
@@ -34,7 +35,18 @@ namespace Matrix42Integrator.Controllers
 		{
 			ClaimsPrincipal principal = ValidateToken(access_token);
 
-			await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+			var authenticationProperties = new AuthenticationProperties();
+			authenticationProperties.StoreTokens(new List<AuthenticationToken>
+			{
+				new AuthenticationToken
+				{
+					Name = OpenIdConnectParameterNames.AccessToken,
+					Value = access_token
+				}
+			});
+
+			await HttpContext.SignInAsync(principal, authenticationProperties);
+
 			return Redirect("/");
 		}
 
